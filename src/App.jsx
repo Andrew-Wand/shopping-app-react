5ee3cb39e7175cef301c55b63becad937448678b
Andrew Wand <andrewwands@gmail.com>
2023-01-31 12:54:32 -0600

    init commit with firebase and tailwind

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import Profile from "./pages/Profile";
+import Shop from "./pages/Shop";
+import Cart from "./pages/Cart";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
+import Navbar from "./components/Navbar";
+import WelcomeHome from "./pages/Welcome";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+
+        {/* Content on page */}
+        <Routes>
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/" element={<WelcomeHome />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/profile" element={<SignIn />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ src/firebase.config.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getFireStore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDPU8xWvDT--RixmRJg5MVxDk1yo9rrigk",
+
+  authDomain: "shopping-app-3af99.firebaseapp.com",
+
+  projectId: "shopping-app-3af99",
+
+  storageBucket: "shopping-app-3af99.appspot.com",
+
+  messagingSenderId: "317240601353",
+
+  appId: "1:317240601353:web:5c43478f417eb4c80e81d8",
+};
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+export const db = getFireStore();
