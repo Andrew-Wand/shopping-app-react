ee266ddcdc5fe5e74b8dfb6a9cc6bab2d83773f5
Andrew Wand <andrewwands@gmail.com>
2023-02-04 20:37:26 -0600

    inital fetch of listings just to access them

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,7 +1,55 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import {
+  collection,
+  getDocs,
+  query,
+  where,
+  orderBy,
+  limit,
+  startAfter,
+} from "firebase/firestore";
+import { db } from "../firebase.config";
+import ShopNav from "../components/ShopNav";
 
 function Shop() {
-  return <div>Shop</div>;
+  const [listings, setListings] = useState(null);
+
+  useEffect(() => {
+    const fetchListings = async () => {
+      try {
+        const listingsRef = collection(db, "listings");
+
+        const q = query(
+          listingsRef,
+          where("category", "==", "tshirt"),
+          limit(10)
+        );
+
+        const querySnap = await getDocs(q);
+
+        let listings = [];
+
+        querySnap.forEach((doc) => {
+          console.log(doc.data());
+        });
+      } catch (error) {
+        console.log("cannot fetch listings");
+      }
+    };
+
+    fetchListings();
+  }, []);
+  return (
+    <div>
+      <div>
+        <ShopNav />
+      </div>
+
+      <main>
+        <div>Shop</div>
+      </main>
+    </div>
+  );
 }
 
 export default Shop;
