5b0f121cc746bcc5261f6c65c26c15d22ab3cba9
Andrew Wand <andrewwands@gmail.com>
2023-02-04 18:11:42 -0600

    can update profile details now

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,14 @@
 import { getAuth, updateProfile } from "firebase/auth";
 import { useEffect, useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
-import { updateDoc } from "firebase/firestore";
+import { updateDoc, doc } from "firebase/firestore";
 import { db } from "../firebase.config";
 
 function Profile() {
   const navigate = useNavigate();
   const auth = getAuth();
+
+  const [changeDetails, setChangeDetails] = useState(false);
 
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
@@ -20,12 +22,71 @@ function Profile() {
     navigate("/");
   };
 
+  const onSubmit = async () => {
+    try {
+      if (auth.currentUser.displayName !== name) {
+        // Update display name in fb
+        await updateProfile(auth.currentUser, {
+          displayName: name,
+        });
+
+        // update in firestore
+        const userRef = doc(db, "users", auth.currentUser.uid);
+        await updateDoc(userRef, {
+          name,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
   return (
     <div>
       <header>
         <p>My Profile</p>
         <button onClick={onLogout}>Logout</button>
       </header>
+
+      <main>
+        <div>
+          <p>Personal Details</p>
+          <p
+            onClick={() => {
+              changeDetails && onSubmit();
+              setChangeDetails((prevState) => !prevState);
+            }}
+          >
+            {changeDetails ? "done" : "change"}
+          </p>
+        </div>
+
+        <div>
+          <form>
+            <input
+              type="text"
+              id="name"
+              disabled={!changeDetails}
+              value={name}
+              onChange={onChange}
+            />
+            <input
+              type="text"
+              id="email"
+              disabled={!changeDetails}
+              value={email}
+              onChange={onChange}
+            />
+          </form>
+        </div>
+      </main>
     </div>
   );
 }
