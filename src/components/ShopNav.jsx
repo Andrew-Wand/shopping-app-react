dfe02413fd2ac24440a107553ea9b8e4c08dafa7
Andrew Wand <andrewwands@gmail.com>
2023-02-06 19:58:56 -0600

    navbar can now change tabs (somewhat)

--- src/components/ShopNav.jsx
+++ src/components/ShopNav.jsx
@@ -1,20 +1,125 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 
 function ShopNav() {
+  // Handle changing genders in the navbar
   const [gender, setGender] = useState("men");
+
+  useEffect(() => {
+    if (gender === "men") {
+      document.querySelector(".men").classList.add("tab-active");
+      document.querySelector(".women").classList.remove("tab-active");
+    }
+
+    if (gender === "women") {
+      document.querySelector(".women").classList.add("tab-active");
+      document.querySelector(".men").classList.remove("tab-active");
+    }
+  }, [gender]);
+
+  const handleSelectGender = (selection) => {
+    if (selection === "men") {
+      setGender("men");
+    }
+
+    if (selection === "women") {
+      setGender("women");
+    }
+  };
+
+  // Handle changing the category in the navbar
+  const [category, setCategory] = useState("tshirt");
+
+  const categoryRef1 = useRef(null);
+  const categoryRef2 = useRef(null);
+  const categoryRef3 = useRef(null);
+  const categoryRef4 = useRef(null);
+
+  useEffect(() => {
+    // document.querySelectorAll(".category-btn").forEach((e) => {
+    //   e.classList.remove("tab-active");
+    // });
+  }, [category]);
+
+  const handleSelectCategory = (selection) => {
+    if (selection === "tshirt") {
+      setCategory("tshirt");
+      categoryRef1.current.classList.add("tab-active");
+      categoryRef2.current.classList.remove("tab-active");
+      categoryRef3.current.classList.remove("tab-active");
+      categoryRef4.current.classList.remove("tab-active");
+    }
+
+    if (selection === "shirt") {
+      setCategory("shirt");
+      categoryRef2.current.classList.add("tab-active");
+      categoryRef1.current.classList.remove("tab-active");
+      categoryRef3.current.classList.remove("tab-active");
+      categoryRef4.current.classList.remove("tab-active");
+    }
+
+    if (selection === "sweater") {
+      setCategory("sweater");
+      categoryRef3.current.classList.add("tab-active");
+      categoryRef1.current.classList.remove("tab-active");
+      categoryRef2.current.classList.remove("tab-active");
+      categoryRef4.current.classList.remove("tab-active");
+    }
+
+    if (selection === "jacket") {
+      setCategory("jacket");
+      categoryRef4.current.classList.add("tab-active");
+      categoryRef1.current.classList.remove("tab-active");
+      categoryRef2.current.classList.remove("tab-active");
+      categoryRef3.current.classList.remove("tab-active");
+    }
+  };
 
   return (
     <nav>
       <div className="tabs">
-        <a className="tab tab-lifted tab-active">Men</a>
-        <a className="tab tab-lifted">Women</a>
+        <button
+          className="men tab tab-lifted tab-active"
+          onClick={() => handleSelectGender("men")}
+        >
+          Men
+        </button>
+        <button
+          className="women tab tab-lifted"
+          onClick={() => handleSelectGender("women")}
+        >
+          Women
+        </button>
       </div>
 
       <div className="tabs tabs-boxed">
-        <a className="tab tab-active">T-Shirts</a>
-        <a className="tab ">Dress Shirts</a>
-        <a className="tab">Sweaters</a>
-        <a className="tab">Jackets</a>
+        <a
+          className="tab tab-active category-btn tshirt"
+          onClick={() => handleSelectCategory("tshirt")}
+          ref={categoryRef1}
+        >
+          {gender === "men" ? "T-Shirts" : "T-Shirts and Tops"}
+        </a>
+        <a
+          className="tab category-btn"
+          onClick={(e) => handleSelectCategory("shirt")}
+          ref={categoryRef2}
+        >
+          {gender === "men" ? "Dress shirts" : "Dresses"}
+        </a>
+        <a
+          className="tab category-btn"
+          onClick={() => handleSelectCategory("sweater")}
+          ref={categoryRef3}
+        >
+          Sweaters
+        </a>
+        <a
+          className="tab category-btn"
+          onClick={() => handleSelectCategory("jacket")}
+          ref={categoryRef4}
+        >
+          Jackets
+        </a>
       </div>
     </nav>
   );
