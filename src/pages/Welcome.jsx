905dfed8eb8bfd629755dd8f4079234d698a9814
Andrew Wand <andrewwands@gmail.com>
2023-02-18 19:53:12 -0600

    info section complete on home page

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -4,6 +4,12 @@ import { collection, getDocs, query, limit, where } from "firebase/firestore";
 import WelcomePicture from "../assets/other/welcome-pic.jpg";
 import { db } from "../firebase.config";
 import FeaturedCard from "../components/FeaturedCard";
+import ModelMen from "../assets/other/model-men-1.jpg";
+import ModelWomen from "../assets/other/model-women-1.jpg";
+import InfoPic1 from "../assets/other/info-pic-1.jpg";
+import InfoPic2 from "../assets/other/info-pic-2.jpg";
+import InfoPic3 from "../assets/other/info-pic-3.jpg";
+import { IoShirtSharp } from "react-icons/io5";
 
 function Welcome() {
   const [featured, setFeatured] = useState(null);
@@ -58,10 +64,13 @@ function Welcome() {
         </div>
       </header>
 
+      {/* FEATURED SECTION */}
       <section>
         <div className="mt-10">
           <h1 className="text-center text-3xl">Featured Items</h1>
-          <div className="divider"></div>
+          <div className="divider">
+            <IoShirtSharp className="text-5xl text-gray-500" />
+          </div>
           <div className="featured-cards grid grid-cols-2 gap-10 p-5">
             {featured?.map((feature) => (
               <FeaturedCard
@@ -74,8 +83,101 @@ function Welcome() {
         </div>
       </section>
 
-      <section>
-        <div></div>
+      {/* PICTURE SECTION */}
+      <section className="mt-32 bg-base-300 w-full h-full">
+        <div className="top flex items-center justify-between w-full">
+          <div className="left flex flex-col max-w-[50%] justify-center p-5 items-start text-lg">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut sequi,
+              voluptate veniam, ratione voluptatum.
+            </p>
+          </div>
+          <div className="right h-full w-6/12">
+            <figure>
+              <img
+                src={ModelMen}
+                alt="Men's Model"
+                className="overflow-hidden object-cover w-[15rem] h-full object-center"
+              />
+            </figure>
+          </div>
+        </div>
+        <div className="divider my-16">
+          <IoShirtSharp className="text-5xl text-gray-500" />
+        </div>
+        <div className="bottom flex items-center justify-between w-full">
+          <div className="left h-full w-6/12">
+            <figure>
+              <img
+                src={ModelWomen}
+                alt="Women's Model"
+                className="overflow-hidden object-cover w-[15rem] h-full object-center"
+              />
+            </figure>
+          </div>
+          <div className="right flex flex-col max-w-[50%] justify-center p-5 items-start text-lg">
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Excepturi voluptatibus tempora.
+            </p>
+          </div>
+        </div>
+      </section>
+
+      {/* INFO SECTION */}
+      <section className="text-center">
+        <h1 className="text-3xl m-12 uppercase font-bold">Who we are</h1>
+        <div>
+          <div>
+            <div className="avatar">
+              <div className="rounded-full h-[15rem] w-[15rem]">
+                <img src={InfoPic1} alt="Pic of clothes" />
+              </div>
+            </div>
+
+            <h2 className="text-2xl font-bold my-5 uppercase">Used Clothing</h2>
+            <p className="p-5">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde eos
+              tempore sapiente quo, nemo, dolore quisquam ab possimus enim
+              aperiam eligendi sequi tenetur. Architecto, quae sunt repellendus
+              itaque voluptas veniam.
+            </p>
+          </div>
+        </div>
+        <div className="mt-16 bg-base-300">
+          <div>
+            <div className="avatar mt-10">
+              <div className="rounded-full h-[15rem] w-[15rem]">
+                <img src={InfoPic2} alt="Pic of clothes" />
+              </div>
+            </div>
+
+            <h2 className="text-2xl font-bold my-5 uppercase">Made to last</h2>
+            <p className="p-5">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde eos
+              tempore sapiente quo, nemo, dolore quisquam ab possimus enim
+              aperiam eligendi sequi tenetur. Architecto, quae sunt repellendus
+              itaque voluptas veniam.
+            </p>
+          </div>
+        </div>
+        <div className="mt-16">
+          <div>
+            <div className="avatar mt-10">
+              <div className="rounded-full h-[15rem] w-[15rem]">
+                <img src={InfoPic3} alt="Pic of clothes" />
+              </div>
+            </div>
+
+            <h2 className="text-2xl font-bold my-5 uppercase">Modern styles</h2>
+            <p className="p-5">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde eos
+              tempore sapiente quo, nemo, dolore quisquam ab possimus enim
+              aperiam eligendi sequi tenetur. Architecto, quae sunt repellendus
+              itaque voluptas veniam.
+            </p>
+          </div>
+        </div>
       </section>
     </div>
   );
