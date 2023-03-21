47820752ab318e8961873421840ce4c4b5488dc5
Andrew Wand <andrewwands@gmail.com>
2023-03-20 19:27:24 -0500

    trying to fix react reveal

--- src/components/FeaturedCard.jsx
+++ src/components/FeaturedCard.jsx
@@ -1,24 +1,21 @@
 import { useNavigate } from "react-router-dom";
-import Fade from "react-reveal/Fade";
 
 function FeaturedCard({ featuredItems, id }) {
   const navigate = useNavigate();
   return (
-    <Fade up cascade>
-      <div
-        className="lg:max-w-[40%] hover:scale-110 transition ease-in tooltip"
-        data-tip={featuredItems.name}
-      >
-        <div>
-          <figure
-            onClick={() => navigate(`/shop/${featuredItems.category}/${id}`)}
-            className="cursor-pointer"
-          >
-            <img src={featuredItems.image} alt="Clothing" />
-          </figure>
-        </div>
+    <div
+      className="lg:max-w-[40%] hover:scale-110 transition ease-in tooltip"
+      data-tip={featuredItems.name}
+    >
+      <div>
+        <figure
+          onClick={() => navigate(`/shop/${featuredItems.category}/${id}`)}
+          className="cursor-pointer"
+        >
+          <img src={featuredItems.image} alt="Clothing" />
+        </figure>
       </div>
-    </Fade>
+    </div>
   );
 }
 
