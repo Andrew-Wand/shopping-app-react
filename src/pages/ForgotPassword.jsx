a34f2693578fddc56009cdd519fef66e0f6862ae
Andrew-Wand <andrewwands@gmail.com>
2023-07-05 13:09:37 -0500

    style: css styling for forgot password page

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -22,25 +22,32 @@ function ForgotPassword() {
   };
 
   return (
-    <div>
+    <div className="lg:flex lg:flex-col lg:items-center h-[500px] lg:h-[800px] lg:mt-20">
+      <Link to="/sign-in" className="btn m-5">
+        Back to Sign In
+      </Link>
       <header>
-        <p>Forgot Password</p>
+        <p className="text-center text-3xl m-5 lg:text-5xl lg:m-10">
+          Forgot Password
+        </p>
       </header>
 
       <main>
         <form onSubmit={onSubmit}>
-          <input
-            type="email"
-            placeholder="Email"
-            id="email"
-            value={email}
-            onChange={onChange}
-          />
-          <Link to="/sign-in">Sign In</Link>
-
-          <div>
-            <div>Send Reset Link</div>
+          <div className="form-control ml-5">
+            <label className="input-group">
+              <span>Email</span>
+              <input
+                type="email"
+                className="input input-bordered w-[69.5%] lg:w-full lg:mr-7"
+                id="email"
+                value={email}
+                onChange={onChange}
+              />
+            </label>
           </div>
+
+          <button className="btn m-5">Send Reset Link</button>
         </form>
       </main>
     </div>
